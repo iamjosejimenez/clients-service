e0de0bc1162be503c899453008eca92348acc5c4
José Jiménez <jljb1990@gmail.com>
2017-07-17 23:23:03 -0400

    Refactorizaciones en el manejo de roles

--- server/boot/get-clients.js
+++ server/boot/get-clients.js
@@ -4,26 +4,24 @@ const {downloadData} = require('../services/data-downloader.js');
 const Promise = require('bluebird');
 const logger = require('../services/logger');
 
-let roles = ['admin', 'user'];
+let {ROLES} = require('../constants.json');
 
 module.exports = (server) => {
   server.enableAuth();
 
   const Role = server.models.Role;
   const RoleMapping = server.models.RoleMapping;
-  return Promise.map(roles, name => {
+  return Promise.map(ROLES, name => {
     return Role.findOrCreate({
       name,
     });
   }).then(rolesCreated => {
-    roles = rolesCreated;
+    ROLES = rolesCreated;
     return downloadData(server);
   }).then(() => {
     const Client = server.models.client;
     return Client.find({});
   }).then(clients => {
-    const adminRole = roles[0][0];
-    const userRole = roles[1][0];
     return Promise.map(clients, client => {
       return RoleMapping.find({
         where: {
@@ -32,18 +30,17 @@ module.exports = (server) => {
         },
       }).then(roleMapping => {
         if (!roleMapping.length) {
-          if (client.role === 'admin') {
-            return adminRole.principals.create({
-              principalType: RoleMapping.USER,
-              principalId: client.id,
-            });
-          } else {
-            return userRole.principals.create({
-              principalType: RoleMapping.USER,
-              principalId: client.id,
-            });
-          }
+          Promise.any(ROLES, ROLE => {
+            if (client.role === ROLE.name) {
+              return ROLE.principals.create({
+                principalType: RoleMapping.USER,
+                principalId: client.id,
+              });
+            }
+            return Promise.reject();
+          });
         }
+        return Promise.resolve();
       });
     });
   }).catch(error => {
--- server/boot/resolvers.js
+++ server/boot/resolvers.js
@@ -1,9 +1,13 @@
 'use strict';
 
+const {ROLES} = require('../constants.json');
+
 module.exports = function(server) {
   const Role = server.models.Role;
-  Role.registerResolver('admin', (role, context, next) => {
-    const client = context.remotingContext.req.client;
-    return next(null, client && client.role === 'admin');
+  ROLES.map(ROLE => {
+    Role.registerResolver(ROLE, (role, context, next) => {
+      const client = context.remotingContext.req.client;
+      return next(null, client && client.role === ROLE);
+    });
   });
 };
