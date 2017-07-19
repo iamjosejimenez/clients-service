430e49529bd5857901d81b49a4a357e8160514b9
José Jiménez <jljb1990@gmail.com>
2017-07-18 23:52:56 -0400

    Refactorizaciones pequenas.

--- server/server.js
+++ server/server.js
@@ -6,9 +6,9 @@ const morgan = require('morgan');
 
 const app = module.exports = loopback();
 
-app.start = function() {
+app.start = () => {
   // start the web server
-  return app.listen(function() {
+  return app.listen(() => {
     app.emit('started');
     const baseUrl = app.get('url').replace(/\/$/, '');
     console.log('Web server listening at: %s', baseUrl);
@@ -21,7 +21,7 @@ app.start = function() {
 
 // Bootstrap the application, configure models, datasources and middleware.
 // Sub-apps like REST API are mounted via boot scripts.
-boot(app, __dirname, function(err) {
+boot(app, __dirname, err => {
   if (err) throw err;
 
   // start the server if `$ node server.js`
